088aa32e4a4a4b78312f05ee635da50b1f50bbab
唐正雄 <tzx1061225829@gmail.com>
2020-05-18 09:54:15 +0800

    Initial Commit

---
+++ irra/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  irra
+//
+//  Created by 唐正雄 on 2020/5/18.
+//  Copyright © 2020 唐正雄. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
